50fc9a7b3c140c54171300fe76b65e722f0b53fe
huxlei <qiaodaima@foxmail.com>
2021-07-16 23:19:18 +0800

    首页添加文章列表显示

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -13,6 +13,20 @@
       <div class="title">首页</div>
       <div class="desc">描述</div>
     </div>
+    <!-- 内容主体 -->
+    <div>
+      <div class="content_zone">
+        <div v-for="(item) in $site.pages" :key="item.key" class="item">
+          <img loading="lazy" src="https://picsum.photos/536/354?random&date=2021-06-26">
+          <div class="text">
+            <!-- 标题 -->
+            <div class="title">{{item.title}}</div>
+            <!-- 摘要 -->
+            <div class="excerpt" v-html="item.excerpt"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,7 +48,7 @@ export default {
 
   created() {
     console.info('site 对象');
-    console.info(this.$site);
+    console.info(this.$site.pages);
   },
 
   mounted() {},
@@ -80,5 +94,35 @@ export default {
       margin-top: 10px;
     }
   }
+  .content_zone {
+    max-width: 1200px;
+    margin: -30px auto 0;
+    .item {
+      min-height: 310px;
+      box-shadow: 0 1px 8px rgb(0 0 0 / 30%);
+      border-radius: 10px;
+      background: #fff;
+      display: flex;
+      margin-bottom: 20px;
+      overflow: hidden;
+      .text {
+        padding: 20px;
+        .title {
+        font-size: 20px;
+        font-weight: bold;
+        width: 100%;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+      }
+      
+    }
+  }
+}
+</style>
+<style>
+body {
+  margin: 0px;
 }
 </style>
